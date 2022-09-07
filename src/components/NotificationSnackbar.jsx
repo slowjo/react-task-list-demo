656c5a9ad1aa2cac12f323c6ccd85843847e503c
slowjo <fabiansen.s83@gmail.com>
2022-09-07 14:04:10 +0300

    notification position fix

--- src/components/NotificationSnackbar.jsx
+++ src/components/NotificationSnackbar.jsx
@@ -3,7 +3,7 @@ import Alert from '@mui/material/Alert';
 
 const NotificationSnackbar = ({ severity, message, open, handleClose }) => {
     return (
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} sx={{ bottom: { xs: '9em', sm: '1em' } }}>
             <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }} variant='filled'>
                 {message}
             </Alert>
